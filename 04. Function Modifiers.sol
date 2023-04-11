0bbbae7a3baee283a9cafe3f0f43401a84a46169
asar30460 <asar30460@gmail.com>
2023-04-11 19:52:34 +0800

    Done for function modifiers

--- 04. Function Modifiers.sol
+++ 04. Function Modifiers.sol
@@ -10,7 +10,7 @@ contract FunctionModifier {
     }
 
     modifier whenNotPaused(){
-        require(!paused, 'Paused.');
+        require(!paused, 'Contract Paused.');
         _;  // This line means run the subsequent code.
     }
 
@@ -18,7 +18,27 @@ contract FunctionModifier {
         count ++;
     }
 
-    function desc() external {
+    function desc() external whenNotPaused{
         count --;
+    }
+
+    modifier restric(uint _x) {
+        require(_x < 100, 'x must less than 100.');
+        _;
+    }
+
+    function incBy(uint _x) external whenNotPaused restric(_x) {
+        count += _x;
+    }
+
+    // Ths rest code is demonstration of sandwich.
+    modifier sandwich() {
+        count += 10;
+        _;
+        count *=2;
+    }
+    // Firstly do count + 10. Then, execute foo function. After end of function, do count * 2;
+    function foo() external sandwich {
+        count += 1;
     }
 }
